ec11f20b24da982e8e6da67e3a908e8d762f6eca
Neeraj Kumar <neeraj.kumar@gmail.com>
2018-03-17 13:20:48 +0530

    initial commit

---
+++ test/periodic_table_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class PeriodicTableTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::PeriodicTable::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
